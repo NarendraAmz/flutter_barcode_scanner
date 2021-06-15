adadc1f80ff79d4f0c4f82362b3eeed74e3f4c63
Narendra <>
2021-06-15 12:36:53 +0530

    flash title stopped.

--- ios/Classes/SwiftFlutterBarcodeScannerPlugin.swift
+++ ios/Classes/SwiftFlutterBarcodeScannerPlugin.swift
@@ -192,8 +192,8 @@ class BarcodeScannerViewController: UIViewController {
     /// Create and return flash button
     private lazy var flashIcon : UIButton! = {
         let flashButton = UIButton()
-        flashButton.setTitle("Flash",for:.normal)
-        flashButton.translatesAutoresizingMaskIntoConstraints=false
+       // flashButton.setTitle("Flash",for:.normal)
+      //  flashButton.translatesAutoresizingMaskIntoConstraints=false
         
         flashButton.setImage(UIImage(named: "ic_flash_off", in: Bundle(identifier: "org.cocoapods.flutter-barcode-scanner"), compatibleWith: nil),for:.normal)
         
